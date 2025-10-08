60bf3dfec3d440eef5cd0705b63362e7faafbad3
Vishnu <vishnuvaradhan@victopialabs.com>
2025-10-08 13:04:11 +0530

    Set up initial API structure views

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,5 +1,8 @@
 import requests
 
-endpoint = "https://httpbin.org/anything"
-get_response = requests.get(endpoint, json={"question": "How are you?"})
-print(get_response.text)
+#endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/api"
+get_response = requests.get(endpoint,params={"abc": 123}, json={"question": "How are you? form py_client"})
+#print(get_response.text)
+#print(get_response.json()["message"])
+#print(get_response.status_code)
