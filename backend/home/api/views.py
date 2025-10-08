4950133a40c3672dedd8abb83272f50558f2af96
Vishnu <vishnuvaradhan@victopialabs.com>
2025-10-08 16:00:09 +0530

    Refactor API response handling in views and update HTTP request in client

--- backend/home/api/views.py
+++ backend/home/api/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+import json
+#from django.http import JsonResponse, HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.forms.models import model_to_dict
 from products.models import Product
 # Create your views here.
+@api_view(['GET'])
 def api_home(request, *args, **kwargs):
+    # DRF
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data['title']= model_data.title,
-        data['content']= model_data.content,
-        data['price']= model_data.price,
-    return JsonResponse(data)
+        data = model_to_dict(model_data, fields=['id', 'title', 'price', 'content'])
+    return Response(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,7 +2,7 @@ import requests
 
 #endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api"
-get_response = requests.get(endpoint,params={"abc": 123}, json={"question": "How are you? form py_client"})
+get_response = requests.get(endpoint,params={"abc": 123})
 #print(get_response.text)
 #print(get_response.json()["message"])
 print(get_response.json())
