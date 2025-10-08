928ef06efaa071f93fd9af0d3c9f3d09fdec4ad2
Vishnu <vishnuvaradhan@victopialabs.com>
2025-10-08 13:43:35 +0530

    Implement Product model and API endpoint to retrieve product data

--- py_client/basic.py
+++ py_client/basic.py
@@ -5,4 +5,4 @@ endpoint = "http://localhost:8000/api"
 get_response = requests.get(endpoint,params={"abc": 123}, json={"question": "How are you? form py_client"})
 #print(get_response.text)
 #print(get_response.json()["message"])
-#print(get_response.status_code)
+print(get_response.json())
