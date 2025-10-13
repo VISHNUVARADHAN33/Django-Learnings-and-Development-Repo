2c95cf2f8d82b022199240c90241472bf2e0286e
Vishnu <vishnuvaradhan@victopialabs.com>
2025-10-13 06:14:06 +0530

    Enhance ProductSerializer and URL routing: add edit URL method and update URL patterns with names for better API navigation

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,11 +1,15 @@
 from rest_framework import serializers
 from products.models import Product
+from rest_framework.reverse import reverse
 
 class ProductSerializer(serializers.ModelSerializer):
     discount = serializers.SerializerMethodField(read_only=True)
+    #url = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)
+    url = serializers.HyperlinkedIdentityField(view_name='product-detail', lookup_field='pk')
     class Meta:
         model = Product
-        fields = ['id', 'title', 'price', 'content', 'Discounted_price', 'discount']
+        fields = ['url','edit_url','id', 'title', 'price', 'content', 'Discounted_price', 'discount']
 
     def get_discount(self, obj):
         if not hasattr(obj, 'id'):
@@ -13,3 +17,17 @@ class ProductSerializer(serializers.ModelSerializer):
         if not isinstance(obj, Product):
             return None
         return obj.get_discount()
+    
+    # def get_url(self, obj):
+    #     request = self.context.get('request')
+    #     if request is None:
+    #         return None
+    #     return reverse('product-detail', kwargs={'pk':obj.pk},request=request)
+
+    def get_edit_url(self, obj):
+        request = self.context.get('request')
+        if request is None:
+            return None
+        return reverse('product-edit', kwargs={'pk':obj.pk},request=request)
+
+
--- backend/products/urls.py
+++ backend/products/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.product_detail_view),
-    path('<int:pk>/update/', views.product_update_view),
+    path('<int:pk>/', views.product_detail_view, name='product-detail'),
+    path('<int:pk>/update/', views.product_update_view, name='product-edit'),
     path('<int:pk>/delete/', views.product_Destroy_view),
-    path('', views.product_list_create_view)
+    path('', views.product_list_create_view, name='product-list')
 ]
