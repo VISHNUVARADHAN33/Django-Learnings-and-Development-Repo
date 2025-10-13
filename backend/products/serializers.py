25aca6d2735c2d7ff1c2c5413b6877a69f7a8de6
Vishnu <vishnuvaradhan@victopialabs.com>
2025-10-13 12:20:52 +0530

    Refactor ProductSerializer and add validators: clean up commented code and introduce unique title validation

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,34 +1,44 @@
 from rest_framework import serializers
 from products.models import Product
 from rest_framework.reverse import reverse
-
+from .validators import validate_title_no_hello, unique_product_title
 class ProductSerializer(serializers.ModelSerializer):
     discount = serializers.SerializerMethodField(read_only=True)
     #url = serializers.SerializerMethodField(read_only=True)
     edit_url = serializers.SerializerMethodField(read_only=True)
     url = serializers.HyperlinkedIdentityField(view_name='product-detail', lookup_field='pk')
+    title = serializers.CharField(validators =[validate_title_no_hello,unique_product_title])
+    #name = serializers.CharField(source="title", read_only=True)                                                                                               -> learned for validation
     #email= serializers.EmailField(write_only=True)                                                   --> RFC comment after model serializer learning
     class Meta:
         model = Product
-        fields = ['url','edit_url','id', 'title', 'price', 'content', 'Discounted_price', 'discount']  #'email', "" " " "  "        "         "
-    # Reason for the comment: This learned on model serializer
-    # def create(self, validated_data):                      
-    #     #email = validated_data.pop('email')  -> Reason for comment this handel on view.py
-    #     obj = super(). create(validated_data)
-    #     #print(email, obj)                    ->    "     "      "    "      "
-    #     return obj
+        fields = ['url','edit_url','id', 'title', 'price', 'content', 'Discounted_price', 'discount']  #'email', "" " " "  "        "         "                      'name',
     
-    # def update(self, instance, validated_data):
-    #     email = validated_data.pop('email')
-    #     return super().update(instance, validated_data)
+    # def validate_title(self, value):
+    #     qs = Product.objects.filter(title__iexact=value)
+    #     if qs.exists():
+    #         raise serializers.ValidationError(f"{value} is already there. give some thing new")
+    #     return value
+    # Reason for the comment: This learned on model serializer
+    """
+    def create(self, validated_data):                      
+        #email = validated_data.pop('email')  -> Reason for comment this handel on view.py
+        obj = super(). create(validated_data)
+        #print(email, obj)                    ->    "     "      "    "      "
+        return obj
+    
+    def update(self, instance, validated_data):
+        email = validated_data.pop('email')
+        return super().update(instance, validated_data)
 
+        """
     def get_discount(self, obj):
         if not hasattr(obj, 'id'):
             return None
         if not isinstance(obj, Product):
             return None
         return obj.get_discount()
-    
+
     # def get_url(self, obj):
     #     request = self.context.get('request')
     #     if request is None:
---
+++ backend/products/validators.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Product
+from rest_framework.validators import UniqueValidator
+
+
+# def validate_title(value):
+#     qs = Product.objects.filter(title__iexact=value)
+#     if qs.exists():
+#         raise serializers.ValidationError(f"{value} is already there. give some thing new")
+#     return value
+
+# def validate_title_no_hello(value):                                                    this are just try out differnet ways
+#     if "iphone" in value.lower():
+#         raise serializers.ValidationError(f"No more iphones are allowed")
+#     return value
+unique_product_title = UniqueValidator(queryset=Product.objects.all())
