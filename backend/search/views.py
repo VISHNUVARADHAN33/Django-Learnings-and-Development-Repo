0bfd4710d6499cf96113a16ba870f81933b9615f
Vishnu <vishnuvaradhan@victopialabs.com>
2025-10-17 10:42:37 +0530

    Add search functionality: implement search client and view for querying products with tags and user filters; update requirements for Algolia package

--- backend/search/views.py
+++ backend/search/views.py
@@ -2,14 +2,29 @@ from rest_framework import generics
 
 from products.models import Product
 from products.serializers import ProductSerializer
+from . import client
+from rest_framework.response import Response
 
 
-class searchListView(generics.ListAPIView):
+class searchListView(generics.GenericAPIView):
+    def get(self, request, *args, **kwargs):
+        user = None
+        if request.user.is_authenticated:
+            user = request.user.username
+        query = request.GET.get('q')
+        public = str(request.GET.get('public')) != "0"
+        tag = request.GET.get('tag') or None
+        if not query:
+            return Response('', status=400)
+        results = client.perform_search(query, tags=tag, user=user, public=public)
+        return Response(results)
+
+class searchListOldView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
     def get_queryset(self, *args, **kwargs):
-        qs = super().get_queryset(*args, **kwargs)  
+        qs = super().get_queryset(*args, **kwargs)
         q = self.request.GET.get('q')
         results = Product.objects.none()
         if q is not None:
